fdfc2f52eb46ff3ddbab6d6e31f620aa87cfff85
Sergey Filyuk <sergey.filyuk@zoomdata.com>
2017-11-27 13:46:20 +0200

    improvements for background terrain

--- bin/landscape.js
+++ bin/landscape.js
@@ -7,14 +7,10 @@ var landScape = new Terrain.LandScape(screen, {
     src: '/img/textureHeightMap3.png'
 });
 
-landScape.display.canvas.resize(screen.offsetWidth, screen.offsetHeight)
-landScape.display.resize(screen.offsetWidth, screen.offsetHeight);
-landScape.camera.ratio = screen.offsetWidth / screen.offsetHeight;
+landScape.resize(screen.offsetWidth,screen.offsetHeight);
 screen.insertBefore(landScape.display.canvas.context,screen.childNodes[0]);
 window.addEventListener('resize', function() {
-    landScape.display.resize(screen.offsetWidth, screen.offsetHeight);
-    landScape.display.canvas.resize(screen.offsetWidth, screen.offsetHeight)
-    landScape.camera.ratio = screen.offsetWidth / screen.offsetHeight;
+    landScape.resize(screen.offsetWidth,screen.offsetHeight);
 });
 
 landScape.start();
